b91cd45866c06950e5c6167bddae58dad4fe1e35
Flaviu <haiduflaviu9@outlook.com>
2025-11-19 18:26:33 +0200

    modificari

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const currentCityTag = document.querySelector(".current-city");
 let currentCity = localStorage.getItem("city");
 
-// Daca nu avem oras salvat in localStorage, salvam orasul default, adica Brasov.
-if (!currentCity) {
-  localStorage.setItem("city", "Brasov");
-  currentCity = "Brasov";
-}
-
 // Actualizam orasul afisat pe ecran.
 currentCityTag.innerHTML = currentCity;
 
@@ -33,4 +27,5 @@ document.addEventListener("scroll", () => {
   } else {
     scrollToTopButton.style.display = "none";
   }
+
 });
